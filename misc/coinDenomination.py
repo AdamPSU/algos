d5889d3cd68f296e6f5832a0088c33f9bd12ee99
Adam Torres Encarnacion <admr0805@gmail.com>
2023-10-12 16:27:52 -0400

    Add misc file and implemented coinDenomination

---
+++ misc/coinDenomination.py
@@ -0,0 +1,15 @@
+coin_denominations = [25, 10, 5, 1]
+
+def make_change(change_needed): 
+    change = []
+    sum = 0 
+    for coin in coin_denominations: 
+        while sum + coin <= change_needed: 
+            sum += coin 
+            change.append(coin) 
+    return change 
+
+
+
+print(make_change(83))
+
